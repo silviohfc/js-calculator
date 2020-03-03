a522da9563401740dd46b5f3edd5d7f860122351
Silvio Henrique <silviohfc@gmail.com>
2020-03-02 21:56:20 -0300

    Created methods to set values on calculator's display

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -4,7 +4,19 @@ class CalcController{
 
         this._displayCalc = "0";
         this._currentDate; 
+        this.initialize();
     }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "01/05/2020";
+        timeEl.innerHTML = "00:00";
+    }
+
 
     get displayCalc(){
         return this._displayCalc;

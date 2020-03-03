fb1c81944cd297e6bdb7aa5e510f0c2f74fc5545
Silvio Henrique <silviohfc@gmail.com>
2020-03-02 21:31:16 -0300

    Project created

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,25 @@
+class CalcController{
+
+    constructor(){
+
+        this._displayCalc = "0";
+        this._currentDate; 
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get currentDate(){
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+        this._currentDate = valor;
+    }
+
+}

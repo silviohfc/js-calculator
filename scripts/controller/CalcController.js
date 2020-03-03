9c3ccf22e266316a39793ee3f986e1290c4145f1
Silvio Henrique <silviohfc@gmail.com>
2020-03-03 02:18:47 -0300

    Created calculating method and display numbers/result

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -67,10 +67,20 @@ class CalcController{
         let result = eval(this._operation.join(''));
 
         this._operation = [result, last];
+        this.setLastNumberToDisplay();
     }
 
     setLastNumberToDisplay(){
-        
+        let lastNumber;
+
+        for(let i = this._operation.length-1; i >= 0; i--){
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this.displayCalc = lastNumber;
     }
 
     addOperation(value){
@@ -88,6 +98,7 @@ class CalcController{
             } else{
 
                 this.pushOperation(value);
+                this.setLastNumberToDisplay();
 
             }
 

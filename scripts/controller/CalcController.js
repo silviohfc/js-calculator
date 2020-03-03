85a398acb3f2220c3ba61f0e0bced453b2cc3964
Silvio Henrique <silviohfc@gmail.com>
2020-03-03 02:32:07 -0300

    Percent calculation and 'equal' function created

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -21,6 +21,7 @@ class CalcController{
             this.setDisplayDateTime();
         }, 1000);
 
+        this.setLastNumberToDisplay();
     }
 
     addEventListenerAll(element, events, fn){
@@ -34,10 +35,12 @@ class CalcController{
 
     clearAll(){
         this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
     cancelEntry(){
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation(){
@@ -62,12 +65,33 @@ class CalcController{
     }
 
     calc(){
-        let last = this._operation.pop();
+
+        let last = '';
+
+        if(this._operation.length > 3){
+            last = this._operation.pop();
+        }
 
         let result = eval(this._operation.join(''));
 
-        this._operation = [result, last];
+        if(last == '%'){
+            
+            result /= 100;
+
+            this._operation = [result];
+
+        } else{
+
+            this._operation = [result];
+
+            if(last){
+                this._operation.push(last);
+            }
+
+        }
+
         this.setLastNumberToDisplay();
+        
     }
 
     setLastNumberToDisplay(){
@@ -78,6 +102,10 @@ class CalcController{
                 lastNumber = this._operation[i];
                 break;
             }
+        }
+
+        if(!lastNumber){
+            lastNumber = 0;
         }
 
         this.displayCalc = lastNumber;
@@ -155,7 +183,7 @@ class CalcController{
                 break;
 
             case 'igual':
-                
+                this.calc();
                 break;
 
             case 'ponto':
